e92ea0967a7002e0bfd0850bfb12145ff39955eb
wfsfdg <rikychoi@gmail.com>
2025-03-24 19:58:11 +0900

    3

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -1,32 +1,42 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.25;
 
 contract escrow{
     address public OwnerAddress;//에스크로 관리자 지갑
+
+    mapping(address => uint256) public deposits; //입금기록
+
     event Deposited(address indexed from, uint256 amount);// 입금 이벤트 로그 (나중에 조회하기위함)
     event Withdrawed(address indexed to, uint256 amount);// 출금 이벤트 로그 
-    event Refunded(address indexed to, uint256 amount);// 환불 이벤트 로그 (나중에 조회하기위함)
+    event Refunded(address indexed to, uint256 amount);// 환불 이벤트 로그
     constructor() 
     {
         OwnerAddress = msg.sender; // 내 지갑주소
     }
 
+
+
     //관리자 지갑으로 입금하는 함수
-    function EscrowDeposit() external payable
+    function EscrowDeposit(uint256 amount) external payable
     {
-        require(msg.value > 0, "Send some ETH");
-        //입금 로직 들어갈곳
+        require(msg.value == amount, "Send required ETH");
+        deposits[msg.sender] += msg.value;
         emit Deposited(msg.sender, msg.value);
     }
-    //관리자 지갑에서 환불하거나 판매자에게 송금하는함수(보안 중요함)
-    function EscrowWithdraw() external payable 
+    //거래성사시 관리자 지갑에서 판매자에게 송금하는함수(보안 중요함)
+    function EscrowWithdraw(address to, uint256 amount) external payable 
     {
 
     }
-    function EscrowRefund() external payable
+    //거래불발시 에치금 환불해주는함수 (보안중요)  2414
+    function EscrowRefund(address to, uint256 amount) external payable
     {
 
     }
 
+}
+
+contract ViewTransactionLog{
+    
 }
