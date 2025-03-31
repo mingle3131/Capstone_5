03b2c83bd5fd8cf018b9ff934d4527a6efa50b9e
seolihan651 <147055391+seolihan651@users.noreply.github.com>
2025-04-01 02:29:37 +0900

    Delete backend directory

--- backend/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cases/', views.all_cases, name='all_cases'),
-    path('item/<int:item_id>', views.auction_detail, name='auction_detail'),
-]
