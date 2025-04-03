723bac844e5861746766994ae3b0578723077bec
wfsfdg <rikychoi@gmail.com>
2025-04-03 14:48:22 +0900

    8

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -10,17 +10,18 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract escrow is Ownable{
 
-    mapping(address => mapping(uint64 => uint256)) public bidsMap;  //입찰자 / 거래번호 / 금액 매핑
-    mapping(address => uint256) public deposits; //입금자 / 금액 매핑
+    mapping(uint64 => mapping(address => uint256)) public bidsMap;  // 거래번호 -> 입찰자 -> 입찰 보증금 
+    mapping(address => uint256) public userBalance; //사용자가 현재 사용가능한 금액
+    mapping(uint64 => address[]) bidderList; // 거래번호별 입찰자목록 저장
     uint256 proceeds = 0; //경매 낙찰로 컨트랙트에 귀속되는 금액
-    enum ActionType { DEPOSIT, WITHDRAW, REFUND }
+    enum ActionType { DEPOSIT, WITHDRAW, REFUND, BID }
     
     
     // 경매 이벤트 로그 (나중에 조회하기위함)
     event BidEvents(address indexed from, uint256 amount, uint256 timestamp, ActionType action);
 
-    //입출금 이벤트 로그
-    event Transactions(address indexed participant, uint256 amount, uint256 timestamp, ActionType action); 
+    //내부 금액 이동 이벤트 로그
+    event Transactions(address indexed user, uint256 amount, uint256 timestamp, ActionType action); 
 
 
     // 아비트럼에서는 이더리움/원화 환율을 못가져옴 -> 이더/달러 달러/원화 가져와서 이중으로 환산함
@@ -47,21 +48,27 @@ contract escrow is Ownable{
         require(msg.value == amount, "Send required ETH");
         //로그인 데이터에 적힌 주소와 실제 주소가 다를시
         require(msg.sender == from, "Check your account"); 
-        deposits[msg.sender] += msg.value;
+        userBalance[msg.sender] += msg.value;
         emit Transactions(msg.sender, msg.value, block.timestamp, ActionType.DEPOSIT);
     }
 
 
     //경매 입찰 함수
-    function Bid(uint64 tradeNum, uint256 amount, address from) external
+    function Bid(uint64 tradeNum, uint256 amount, address bidder) external
     {
-
+        require(amount < userBalance[bidder], "Not enough balance");
+        bidsMap[tradeNum][bidder] += amount;
+        userBalance[bidder] -=amount;
+        bidderList[tradeNum].push(bidder);
+        emit BidEvents(bidder, amount, block.timestamp, ActionType.BID);
     }
 
 
     //거래성사시 에치금 환불해주는함수
-    function EscrowRefund(uint256 amount, uint64 tradeNum) external payable
+    function EscrowRefund(uint256 amount, uint64 tradeNum, address winner) external 
     {
+        proceeds += amount;
+        
 
     }
 
@@ -72,10 +79,11 @@ contract escrow is Ownable{
     }
 
     //경매 운영자가 저장된 금액 회수하는함수
-    function Collectproceeds(uint256 amount) external onlyOwner
+    function Collectproceeds() external payable onlyOwner
     {
-        require(proceeds >= amount, "Not enough balance");
-        payable(owner()).transfer(amount);
+        require(proceeds > 0, "Not enough balance");
+        payable(owner()).transfer(proceeds);
+        proceeds = 0; 
     }
 }
 
