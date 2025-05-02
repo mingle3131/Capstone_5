aed4a7581f3590c2d61e6cfe8600445927c77e4f
Leeminji <mingle3131@naver.com>
2025-05-02 22:47:57 +0900

    back_autobid

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('tender/', views.tender, name='tender'),
     path('charge/', views.charge, name='charge'),
     path('auto_bid/', views.auto_bid, name='auto_bid'),
+    path('api/auto-bid/', views.auto_bid_reservation, name='auto_bid_reservation'),
+    path('api/search-property/', views.search_property, name='search_property'),
+    path('api/favorites/', views.get_favorite_properties, name='get_favorites'), #즐겨찾기
 ]
 
